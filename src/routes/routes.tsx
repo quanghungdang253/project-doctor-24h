ec69084ae5a5a6a8abcb95f86567b49e02734b68
QuangHung <quanghungdang253@gmail.com>
2025-07-04 15:10:31 +0700

    update

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,7 +1,6 @@
 
 import Home from "../pages/home/home";
-import VdParents from "../vi-du-thuc-hanh/vd-parent";
-import Vd from "../vi-du-thuc-hanh/vd";
+
 const ListRoute = [
     {
         path: '/',
