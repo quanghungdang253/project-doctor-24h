28077c504002ad2b7b80dd71b2b8f29a554d57bf
QuangHung <quanghungdang253@gmail.com>
2025-07-04 15:00:56 +0700

    update

--- src/common/header/components/header-categry.tsx
+++ src/common/header/components/header-categry.tsx
@@ -1,90 +1,166 @@
-import  { useEffect, useState } from 'react';
+// import  { useEffect, useState } from 'react';
+// import Menu from '../data/data-menu';
+// import IconUser from '../../../ui/icon-user';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
+// import { ClipLoader } from "react-spinners";
+// import HeaderMenu from './header-menu';
+// import type  { mainMenu } from '../data/data-menu';
+
+
+// const HeaderCategory: React.FC = () => {
+    
+//     const [product, setProduct] = useState<mainMenu | null>(null);
+//     const [index , setIndex] = useState<number>(0);
+//     const [openForm, setOpenForm] = useState(false);
+// useEffect(() => {
+// function handleIndex():void {
+//         const handleFind = Menu.find((item) => item.id === index);
+//         if(handleFind){
+//             console.log(handleFind);
+//                 setProduct(handleFind);
+//         }
+// }
+//         handleIndex()
+// },[index])
+
+// const handleClick = (
+//     _event: React.MouseEvent<HTMLDivElement> 
+//   , index: number , openform: boolean) => {
+//         setIndex(index);
+//         setOpenForm(openform);
+        
+// }
+//     return (
+//         <div>
+//             <div className='pb-2 pt-2'>
+//                  <div className='flex justify-between'>
+//                     <h1 className='font-bold text-[1.5em] font-roboto'> Doctor24h </h1>
+//                     <div className='flex '>
+//                         {Menu.map((item) => (
+//                             <div 
+//                                 className={`pr-5 pl-5 font-bold  cursor-pointer text-[14px]   flex gap-1 items-center 
+//                                 ${(item.id === index && openForm) ? 'bg-green-700 rounded-2xl text-white' : ""}
+//                                 `}
+//                                 key={item.id}
+//                                 onClick={ (e) => handleClick(e, item.id,true)}>                         
+//                               {item.name}
+//                             <FontAwesomeIcon icon={faCaretDown} />
+//                             </div>
+//                         ))}
+//                            <div className='pr-5 pl-5 cursor-pointer flex    items-center    font-bold rounded-xl'>
+//                             <IconUser />
+//                              <span>   Đăng nhập    </span>
+
+//                           </div>
+//                     </div>
+                         
+//                  </div>
+//                </div>
+//                           <hr  className='opacity-20'/>
+//             <div>
+//                 {
+//                     !product  ? (
+//                             <ClipLoader color="blue" loading={true} size={150} />
+//                     ) : (
+//                      <div>  
+                          
+//                             {openForm ? (
+//                                 <div className=' '>  
+//                                         <HeaderMenu dataMenu={product} closeForm={() => setOpenForm(false)}/>
+//                                 </div>
+                                
+//                             ) : (
+//                                     null  
+//                             )
+//                             }
+//                         </div>
+//                     )
+//                 }
+//                     </div>
+
+//             </div>
+      
+
+// )}
+        
+
+
+
+// export default HeaderCategory;
+
+
+import { useEffect, useState } from 'react';
 import Menu from '../data/data-menu';
 import IconUser from '../../../ui/icon-user';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 import { ClipLoader } from "react-spinners";
 import HeaderMenu from './header-menu';
-import type  { mainMenu } from '../data/data-menu';
-
+import type { mainMenu } from '../data/data-menu';
 
 const HeaderCategory: React.FC = () => {
-    
-    const [product, setProduct] = useState<mainMenu | null>(null);
-    const [index , setIndex] = useState<number>(0);
-    const [openForm, setOpenForm] = useState(false);
-useEffect(() => {
-function handleIndex():void {
-        const handleFind = Menu.find((item) => item.id === index);
-        if(handleFind){
-            console.log(handleFind);
-                setProduct(handleFind);
-        }
-}
-        handleIndex()
-},[index])
+  const [product, setProduct] = useState<mainMenu | null>(null);
+  const [selectedIndex, setSelectedIndex] = useState<number>(0);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-const handleClick = (
-    _event: React.MouseEvent<HTMLDivElement> 
-  , index: number , openform: boolean) => {
-        setIndex(index);
-        setOpenForm(openform);
-        
-}
-    return (
-        <div>
-            <div className='pb-2 pt-2'>
-                 <div className='flex justify-between'>
-                    <h1 className='font-bold text-[1.5em] font-roboto'> Doctor24h </h1>
-                    <div className='flex '>
-                        {Menu.map((item) => (
-                            <div 
-                                className={`pr-5 pl-5 font-bold  cursor-pointer text-[14px]   flex gap-1 items-center 
-                                ${(item.id === index && openForm) ? 'bg-green-700 rounded-2xl text-white' : ""}
-                                `}
-                                key={item.id}
-                                onClick={ (e) => handleClick(e, item.id,true)}>                         
-                              {item.name}
-                            <FontAwesomeIcon icon={faCaretDown} />
-                            </div>
-                        ))}
-                           <div className='pr-5 pl-5 cursor-pointer flex    items-center    font-bold rounded-xl'>
-                            <IconUser />
-                             <span>   Đăng nhập    </span>
+  useEffect(() => {
+    const selectedItem = Menu.find((item) => item.id === selectedIndex);
+    if (selectedItem) {
+      setProduct(selectedItem);
+    }
+  }, [selectedIndex]);
 
-                          </div>
-                    </div>
-                         
-                 </div>
-               </div>
-                          <hr  className='opacity-20'/>
-            <div>
-                {
-                    !product  ? (
-                            <ClipLoader color="blue" loading={true} size={150} />
-                    ) : (
-                     <div>  
-                          
-                            {openForm ? (
-                                <div className=' '>  
-                                        <HeaderMenu dataMenu={product} closeForm={() => setOpenForm(false)}/>
-                                </div>
-                                
-                            ) : (
-                                    null  
-                            )
-                            }
-                        </div>
-                    )
-                }
-                    </div>
+  const handleClick = (_e: React.MouseEvent<HTMLDivElement>, index: number) => {
+    setSelectedIndex(index);
+    setIsMenuOpen(true);
+  };
 
+  return (
+    <div>
+      {/* Header Top */}
+      <div className="pb-2 pt-2">
+        <div className="flex justify-between">
+          <h1 className="font-bold text-[1.5em] font-roboto">Doctor24h</h1>
+          <div className="flex items-center">
+            {/* Menu Items */}
+            {Menu.map((item) => (
+              <div
+                key={item.id}
+                className={`pr-5 pl-5 font-bold cursor-pointer text-[14px] flex gap-1 items-center
+                  ${item.id === selectedIndex && isMenuOpen ? 'bg-green-700 rounded-2xl text-white' : ''}`}
+                onClick={(e) => handleClick(e, item.id)}
+              >
+                {item.name}
+                <FontAwesomeIcon icon={faCaretDown} />
+              </div>
+            ))}
+
+            {/* Login Button */}
+            <div className="pr-5 pl-5 cursor-pointer flex items-center font-bold rounded-xl">
+              <IconUser />
+              <span>Đăng nhập</span>
             </div>
-      
+          </div>
+        </div>
+      </div>
 
-)}
-        
+      <hr className="opacity-20" />
 
-
+      {/* Menu Content */}
+      <div>
+        {!product ? (
+          <ClipLoader color="blue" loading={true} size={150} />
+        ) : (
+          isMenuOpen && (
+            <div>
+              <HeaderMenu dataMenu={product} closeForm={() => setIsMenuOpen(false)} />
+            </div>
+          )
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default HeaderCategory;
--- src/common/header/components/header-menu.tsx
+++ src/common/header/components/header-menu.tsx
@@ -1,86 +1,169 @@
 
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faXmark} from '@fortawesome/free-solid-svg-icons';
+// import { Link } from 'react-router-dom';
+
+//   interface dataLeft {
+//         id: number,
+//         content: string,
+//         img: string,
+//   }
+//   interface dataMenu {
+//         id: number;
+//         name: string;
+//         icon: string;
+//         title: string;
+//         dataleft: dataLeft[]
+//     }
+//     interface headerMenu {
+//         dataMenu : dataMenu;
+//         closeForm: () => void;
+//     }
+    
+//     // cách 1 : props: headerMenu
+// function HeaderMenu({dataMenu, closeForm}: headerMenu ) {
+
+//     return (
+//         <div className='bg-white pb-6 pt-4'>
+//              <div className='pl-10 relative'>
+//                 <big onClick={closeForm} className='absolute top-0 right-0 cursor-pointer pr-6'> 
+//                         <FontAwesomeIcon icon={faXmark} />
+//                 </big>
+            
+//                 <div className=' flex'>
+//                   <div className='w-[15em]'>   
+//                     <h1 className=' text-[18px] font-bold pb-4 pt-2'> {dataMenu.title} </h1>
+//                        <div className='grid gap-10'>
+//                           {dataMenu.dataleft.map((item) => (
+//                             <Link to={item.link} 
+//                                 className={`
+//                                     flex 
+//                                     items-center
+//                                     gap-4  
+                                 
+//                                   text-black
+//                                     cursor-pointer
+//                                     text-opacity-50 
+//                                     ${item.id === dataMenu.dataleft.length - 1 ? 'border-blue-600  font-bold justify-start text-[12px] outline-1 outline-blue-600 p-2 rounded-t-sm' : ' text-[16px]'}
+//                                 `}>
+//                                 <img src={item.img} alt="" className='max-w-10 rounded-2xl '/>
+//                                 {item.content}
+//                             </Link>
+//                          ))}
+//                         </div>
+//                       </div>
+//                         <div className=' w-[1px] opacity-25 bg-black ml-10'></div>
+//                         <div className='mt-4 ml-8'>
+//                                  <h1
+//                                  className='text-[12px] h-[3em]'
+//                                  >{dataMenu.title2}
+//                                      <Link to={}
+//                                         className='text-red-600 inline-block p-2 font-bold  rounded-3xl'> 
+//                                         {dataMenu.viewNow} <FontAwesomeIcon icon={dataMenu.icon}
+//                                       />  
+//                                     </Link>
+//                                 </h1>
+//                          <div className='flex gap-6 mt-2.5'>  
+//                          {dataMenu.dataRight.map((item) => (
+//                                 <div key={item.id}> 
+//                                     <Link>
+//                                         <img src={item.img} alt="" className='max-w-[13em] ' />
+//                                     </Link>
+//                                 </div>
+                      
+//                          ))}
+//                          </div>
+
+//                     </div>
+                
+//                 </div>
+//              </div>
+       
+//         </div>
+//     );
+// }
+
+// export default HeaderMenu;
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faXmark,faRightLong } from '@fortawesome/free-solid-svg-icons';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 
-  interface dataLeft {
-        id: number,
-        content: string,
-        img: string,
-  }
-  interface dataMenu {
-        id: number;
-        name: string;
-        icon: string;
-        title: string;
-        dataleft: dataLeft[]
-    }
-    interface headerMenu {
-        dataMenu : dataMenu;
-        closeForm: () => void;
-    }
-    
-    // cách 1 : props: headerMenu
-function HeaderMenu({dataMenu, closeForm}: headerMenu ) {
 
-    return (
-        <div className='bg-white pb-6 pt-4'>
-             <div className='pl-10 relative'>
-                <big onClick={closeForm} className='absolute top-0 right-0 cursor-pointer pr-6'> 
-                        <FontAwesomeIcon icon={faXmark} />
-                </big>
-            
-                <div className=' flex'>
-                  <div className='w-[15em]'>   
-                    <h1 className=' text-[18px] font-bold pb-4 pt-2'> {dataMenu.title} </h1>
-                       <div className='grid gap-10'>
-                          {dataMenu.dataleft.map((item) => (
-                            <Link to={item.link} 
-                                className={`
-                                    flex 
-                                    items-center
-                                    gap-4  
-                                 
-                                  text-black
-                                    cursor-pointer
-                                    text-opacity-50 
-                                    ${item.id === dataMenu.dataleft.length - 1 ? 'border-blue-600  font-bold justify-start text-[12px] outline-1 outline-blue-600 p-2 rounded-t-sm' : ' text-[16px]'}
-                                `}>
-                                <img src={item.img} alt="" className='max-w-10 rounded-2xl '/>
-                                {item.content}
-                            </Link>
-                         ))}
-                        </div>
-                      </div>
-                        <div className=' w-[1px] opacity-25 bg-black ml-10'></div>
-                        <div className='mt-4 ml-8'>
-                                 <h1
-                                 className='text-[12px] h-[3em]'
-                                 >{dataMenu.title2}
-                                     <Link 
-                                        className='text-red-600 inline-block p-2 font-bold  rounded-3xl'> 
-                                        {dataMenu.viewNow} <FontAwesomeIcon icon={dataMenu.icon}
-                                      />  
-                                    </Link>
-                                </h1>
-                         <div className='flex gap-6 mt-2.5'>  
-                         {dataMenu.dataRight.map((item) => (
-                                <div key={item.id}> 
-                                    <Link>
-                                        <img src={item.img} alt="" className='max-w-[13em] ' />
-                                    </Link>
-                                </div>
-                      
-                         ))}
-                         </div>
+interface dataLeft {
+  id: number;
+  content: string;
+  img: string;
+  link: string;
+}
 
-                    </div>
-                
+interface dataRight {
+  id: number;
+  img: string;
+}
+
+interface dataMenu {
+  id: number;
+  name: string;
+  icon: "";
+  title: string;
+  title2: string;
+  viewNow: string;
+  dataleft: dataLeft[];
+  dataRight: dataRight[];
+}
+
+interface HeaderMenuProps {
+  dataMenu: dataMenu;
+  closeForm: () => void;
+}
+
+function HeaderMenu({ dataMenu, closeForm }: HeaderMenuProps) {
+  return (
+    <div className='bg-white pb-6 pt-4'>
+      <div className='pl-10 relative'>
+        <big onClick={closeForm} className='absolute top-0 right-0 cursor-pointer pr-6'>
+          <FontAwesomeIcon icon={faXmark} />
+        </big>
+
+        <div className='flex'>
+          <div className='w-[15em]'>
+            <h1 className='text-[18px] font-bold pb-4 pt-2'>{dataMenu.title}</h1>
+            <div className='grid gap-10'>
+              {dataMenu.dataleft.map((item) => (
+                <Link to={item.link} key={item.id} className={`flex items-center gap-4 text-black text-opacity-50 cursor-pointer ${item.id === dataMenu.dataleft.length - 1 ? 'border-blue-600 font-bold justify-start text-[12px] outline-1 outline-blue-600 p-2 rounded-t-sm' : 'text-[16px]'}`}>
+                  <img src={item.img} alt="" className='max-w-10 rounded-2xl' />
+                  {item.content}
+                </Link>
+              ))}
+            </div>
+          </div>
+
+          <div className='w-[1px] opacity-25 bg-black ml-10'></div>
+
+          <div className='mt-4 ml-8'>
+            <h1 className='text-[12px] h-[3em]'>
+              {dataMenu.title2}
+              <Link to="/" className='text-red-600 inline-block p-2 font-bold rounded-3xl'>
+                {dataMenu.viewNow}
+                {/* <FontAwesomeIcon icon={dataMenu.icon} /> */}
+              </Link>
+            </h1>
+
+            <div className='flex gap-6 mt-2.5'>
+              {dataMenu.dataRight.map((item) => (
+                <div key={item.id}>
+                  <Link to="/">
+                    <img src={item.img} alt="" className='max-w-[13em]' />
+                  </Link>
                 </div>
-             </div>
-       
+              ))}
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default HeaderMenu;
